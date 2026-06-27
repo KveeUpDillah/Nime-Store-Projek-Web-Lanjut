0eb0416cdfa90a0f343da9f875a4e9852210bdc4
Habibndutt <khunagueroagnes130706@gmail.com>
2026-06-27 19:26:51 +0800

    memisahkan beberapa style dan js ke folder static agar tidak menumpuk di html dan gampang untuk melakukan debug jika terdapat masalah

--- anime_store/settings.py
+++ anime_store/settings.py
@@ -116,6 +116,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'anime_store' / 'static',
+]
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
